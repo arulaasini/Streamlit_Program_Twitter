932e27289f0ca91083c0011f8665b0e0a43d1a19
A Vajravelu <121093195+arulaasini@users.noreply.github.com>
2023-01-26 13:51:13 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import snscrape.modules.twitter as sntwitter
+import json
+# import pymongo
+# from pymongo import MongoClient
+import streamlit as st
+
+st.title("""
+Scraping Data from Twitter Using Python
+""")
+st.write("""
+This app scrapes the data from the Twitter
+""")
+st.write ("---")
+st.write('**Description of Dataset**')
+st.write("""
+The dataset includes id, date, url, username, content, replycount, retweetcount,
+likecount, language and source of the tweets
+""")
+
+#Inputs the number of tweets and username from the user
+limit = st.number_input('Enter the limit:' )
+username = st.text_input('Enter the username without @ symbol:')
+lst1 = []
+for tweet in sntwitter.TwitterSearchScraper(username).get_items():
+    if len(lst1) == limit:
+        break
+    else:
+        lst1.append([tweet.date, tweet.url, tweet.content, tweet.id, tweet.user.username, tweet.replyCount,
+                         tweet.retweetCount, tweet.likeCount, tweet.lang, tweet.source])
+
+#loading into pandas dataframe
+df = pd.DataFrame(lst1, columns=["Date", "url", "content", "id", "username", "replyCount", "retweetCount", "likeCount",
+                               "language", "source"])
+st.write(df)
+
+#type of files either json or csv
+fileselect = st.sidebar.selectbox(
+    label = "Select the type of the file you want",
+    options = ["CSV", "json"])
+
+if fileselect == "CSV":
+    df.to_csv('df_csv.csv', sep=',')
+    CSV = pd.read_csv('df_csv.csv')
+    st.write(CSV)
+    st.download_button("Download CSV", df.to_csv(), mime = 'text/csv')
+if fileselect == "json":
+    dfj = df.to_json(orient='records')
+    st.write(dfj)
+    from pathlib import Path
+    st.download_button(
+    "Download json",
+    data=Path("dfj.json").read_text(),
+    file_name="dfj.json",
+    mime="application/json",)
